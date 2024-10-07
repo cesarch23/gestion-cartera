adfa7775872fc96973b9dec73413dbddac31ea2d
cesarch23 <127994013+cesarch23@users.noreply.github.com>
2024-10-07 00:21:13 +0000

    Deploying to gh-pages from @ cesarch23/gestion-cartera@25bd61d380b42517f94f65cd201bee43b899507d 🚀

--- src/app/auth/layout-auth/layout-auth.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-layout-auth',
-  templateUrl: './layout-auth.component.html'
-})
-export class LayoutAuthComponent {
-
-}
