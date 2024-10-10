9c2ab5641c041f32f28c312bb4e976637edc14fa
Cesar Choccata <c21choccata@gmail.com>
2024-10-10 14:56:11 -0500

    Add custom formats to manage date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+
+const CUSTOM_FORMAT = {
+  parse: {
+    dateInput: 'dd MMM yyyy',
+  },
+  display: {
+    dateInput: 'dd MMM yyyy',
+    monthYearLabel: 'MMM yyyy',
+    dateA11yLabel: 'dd MMM yyyy',
+    monthYearA11yLabel: 'MMMM yyyy',
+  },
+}
 
 @NgModule({
   declarations: [
@@ -15,10 +29,15 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    MatMomentDateModule,
+
    
   ],
-  providers: [],
+  providers: [ 
+    { provide: MAT_DATE_FORMATS, useValue:CUSTOM_FORMAT },
+    { provide: MAT_DATE_LOCALE, useValue:'es-ES' },
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
